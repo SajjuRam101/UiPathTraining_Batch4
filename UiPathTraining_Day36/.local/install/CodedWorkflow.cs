6c799dc46513dc89630318d7840e08f12a06d8d9
Siva Ramakrishna Reddy Karri <karri.sivaram@hotmail.com>
2023-06-28 22:32:10 -0400

    Changes on 28th Jun

---
+++ UiPathTraining_Day36/.local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace UiPathTraining_Day36
+{
+    public class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
