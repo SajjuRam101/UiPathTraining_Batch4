4131539f08fcd53402614c1ebb62888508ca3554
Siva Ramakrishna Reddy Karri <karri.sivaram@hotmail.com>
2023-07-06 21:55:49 -0500

    Changes on 6th July

--- UiPathTraining_Day39/.local/install/CodedWorkflow.cs
+++ UiPathTraining_Day39/.local/install/CodedWorkflow.cs
@@ -13,19 +13,19 @@ using UiPath.UIAutomationNext.API.Contracts;
 using UiPath.UIAutomationNext.API.Models;
 using UiPath.UIAutomationNext.Enums;
 
-namespace UiPathTraining_Day37
+namespace UiPathTraining_Day39
 {
     public class CodedWorkflow : CodedWorkflowBase
     {
         public CodedWorkflow()
         {
-            _ = new System.Type[]{typeof(UiPath.Testing.API.ITestingService), typeof(UiPath.Core.Activities.API.ISystemService), typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService)};
+            _ = new System.Type[]{typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService), typeof(UiPath.Core.Activities.API.ISystemService), typeof(UiPath.Testing.API.ITestingService)};
         }
 
-        protected UiPath.Testing.API.ITestingService testing { get => serviceContainer.Resolve<UiPath.Testing.API.ITestingService>() ; }
+        protected UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService uiAutomation { get => serviceContainer.Resolve<UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService>() ; }
 
         protected UiPath.Core.Activities.API.ISystemService system { get => serviceContainer.Resolve<UiPath.Core.Activities.API.ISystemService>() ; }
 
-        protected UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService uiAutomation { get => serviceContainer.Resolve<UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService>() ; }
+        protected UiPath.Testing.API.ITestingService testing { get => serviceContainer.Resolve<UiPath.Testing.API.ITestingService>() ; }
     }
 }
---
+++ UiPathTraining_Day39/.local/install/Workflow+WorkflowActivity.cs
@@ -0,0 +1,58 @@
+using System.Activities;
+using UiPath.CodedWorkflows;
+using UiPath.CodedWorkflows.Utils;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using UiPath.Core;
+using UiPath.Core.Activities.Storage;
+using UiPath.Orchestrator.Client.Models;
+using UiPath.Testing;
+using UiPath.Testing.Activities.TestData;
+using UiPath.Testing.Activities.TestDataQueues.Enums;
+using UiPath.Testing.Enums;
+using UiPath.UIAutomationNext.API.Contracts;
+using UiPath.UIAutomationNext.API.Models;
+using UiPath.UIAutomationNext.Enums;
+
+namespace UiPathTraining_Day39
+{
+    public class WorkflowActivity : Activity
+    {
+        public WorkflowActivity()
+        {
+            this.Implementation = () =>
+            {
+                return new WorkflowActivityChild()
+                {};
+            };
+        }
+    }
+
+    internal class WorkflowActivityChild : CodeActivity
+    {
+        public WorkflowActivityChild()
+        {
+            DisplayName = "Workflow";
+        }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var codedWorkflow = new UiPathTraining_Day39.Workflow();
+            CodedWorkflowHelper.Initialize(codedWorkflow, context);
+            CodedWorkflowHelper.RunWithExceptionHandling(() =>
+            {
+                codedWorkflow.Before(new BeforeRunContext()
+                {RelativeFilePath = "Workflow.cs"});
+            }, () =>
+            {
+                codedWorkflow.Execute();
+                return null;
+            }, (exception, outArgs) =>
+            {
+                codedWorkflow.After(new AfterRunContext()
+                {RelativeFilePath = "Workflow.cs", Exception = exception});
+            });
+        }
+    }
+}
